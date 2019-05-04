ad061e7dae388c20934efac326640f337f06fc2d
Fredrik Sellgren <Fredrik.sellgren89@gmail.com>
2019-05-04 14:15:29 +0200

    Initial Commit

---
+++ lab3_algor/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lab3_algor
+//
+//  Created by Fredrik Sellgren on 2019-05-04.
+//  Copyright © 2019 Fredrik Sellgren. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
